3d545b6ed2944d068ccd0e89d29431b561330b76
Denkschmied <thomas.schmiedinger@fh-kufstein.ac.at>
2022-12-01 08:27:19 +0100

    Initial commit

---
+++ dayOne.rb
@@ -0,0 +1,17 @@
+# Define empty array
+b = []
+
+# Open File
+file = File.open('Day1/input.txt')
+
+# Read content of file
+file_data = file.read
+
+# Split the data (input was a string); process ist for each line
+file_data.split(/\n{2,}/).each do |line|
+  # Each single line is split based on the delimiter \n; conversion to integer; calculation of sum
+  b << line.split(/\n/).map(&:to_i).sum
+end
+
+# Select the maximum values (in this case the 3 maximum values); calculate the sum of the maximum values
+puts b.max(3).sum
